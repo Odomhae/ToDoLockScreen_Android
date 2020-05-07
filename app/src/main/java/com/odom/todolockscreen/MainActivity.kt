64d0a6a2bde0f996f357e994a9af18d9dc78f591
unknown <jihoon708@naver.com>
2020-05-07 22:58:33 +0900

    12th

--- app/src/main/java/com/odom/todolockscreen/MainActivity.kt
+++ app/src/main/java/com/odom/todolockscreen/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.odom.todolockscreen
 
-import android.Manifest
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.PixelFormat
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -19,36 +16,19 @@ import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import android.widget.Toast
 import android.view.KeyEvent.KEYCODE_ENTER
-import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.net.Uri
 import android.provider.Settings
-import android.webkit.PermissionRequest
-import androidx.core.content.ContextCompat.startActivity
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     // 빈 데이터 리스트 생성.
     val items = ArrayList<String>()
-
     //퍼미션 응답 처리 코드
     private val multiplePermissionsCode = 100
-
     //  하나의? 여려개 선택도 가능한게 나은데 // 이 가능한  adapter 설정
     val adapter by lazy {  ArrayAdapter(this, android.R.layout.select_dialog_item, items) }
 
-    private val requiredPermissions = arrayOf(
-         Manifest.permission.SYSTEM_ALERT_WINDOW)
-
-    private var permissionCodes = arrayOf(
-        multiplePermissionsCode
-
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,21 +37,10 @@ class MainActivity : AppCompatActivity() {
         window.statusBarColor = resources.getColor(R.color.colorGray)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
-        if (!Settings.canDrawOverlays(this)) {
-            // You don't have permission
-            Log.d("rnjsgks", "권한 ㄴㄴ")
-            checkPermission4()
-        }
-
-        // 다른 거 위에 뜨는게 허용 안되있으면
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            val myIntent =  Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-//            myIntent.data = Uri.parse("package:$packageName")
-//            startActivity(myIntent)
-//            // 뜨고 허용했을때 앱 실행되고
-//        }
-        // 허용되있으면 그냥 바로 메인뜨고..
-
+        // 확인해보고
+        // 다른 거 위에 뜨는게 허용 안되있으면 띄우고
+        // 허용되있으면 바로 실행
+        checkPermission()
 
         // 설정
         settingButton.setOnClickListener {
@@ -115,8 +84,6 @@ class MainActivity : AppCompatActivity() {
 
         // 당겨서 새로고침
         pullToRefresh.setOnRefreshListener {
-            //삭제된게 반영된 배열을 불러와야하니까
-            // 기존에 있던 거 없애고
             items.clear()
             adapter.notifyDataSetChanged()
             //  다시 채움
@@ -127,94 +94,34 @@ class MainActivity : AppCompatActivity() {
             }
 
             adapter.notifyDataSetChanged()
-            // 선택 초기화
-            listView.clearChoices()
             pullToRefresh.isRefreshing = false
         }
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == 123) {
-            if (!Settings.canDrawOverlays(this)) {
-                // You don't have permission
-                checkPermission4()
-            } else {
-                // Do as per your logic
-                Log.d("아씨바", "권한없으")
-            }
-
-        }
-    }
-
-
-    fun checkPermission4() {
+    fun checkPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (!Settings.canDrawOverlays(this)) {
                 val intent =  Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                     Uri.parse("package:$packageName"))
-                startActivityForResult(intent, 123)
+                startActivityForResult(intent, multiplePermissionsCode)
             }
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-    fun checkPermission3(){
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            // 다시 보지 않기 버튼을 만드려면 이 부분에 바로 요청을 하도록 하면 됨 (아래 else{..} 부분 제거)
-            // ActivityCompat.requestPermissions((Activity)mContext, new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSION_CAMERA);
-
-            // 처음 호출시엔 if()안의 부분은 false로 리턴 됨 -> else{..}의 요청으로 넘어감
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                AlertDialog.Builder(this)
-                        .setTitle("알림")
-                        .setMessage("저장소 권한이 거부되었습니다. 사용을 원하시면 설정에서 해당 권한을 직접 허용하셔야 합니다.")
-                        .setNeutralButton("설정"
-                        ) { dialogInterface, i ->
-                        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-                        intent.data = Uri.parse("package:" + packageName)
-                        startActivity(intent)
-                        }
-                        .setPositiveButton("확인"
-                        ) { dialogInterface, i -> finish() }
-                        .setCancelable(false)
-                        .create()
-                        .show()
-            } else {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), 101)
-            }
-        }
-    }
-
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (Build.VERSION.SDK_INT >= 23) {
-            if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
-                Log.d("Tag11", "Permission: "+permissions[0]+ "was "+grantResults[0])
-                //resume tasks needing this permission
+        if (requestCode == multiplePermissionsCode) {
+            if (Settings.canDrawOverlays(this)) {
+                Log.d("TAG", "권한부여 완료")
+                Toast.makeText(applicationContext, R.string.permission_set_message, Toast.LENGTH_LONG).show()
+                onResume()
             }else{
-                Log.d("Tag22", "Permission: "+permissions[0]+ "was "+grantResults[0])
-                AlertDialog.Builder(this)
-                    .setTitle("알림")
-                    .setMessage("저장소 권한이 22거부되었습니다. 사용을 원하시면 설정에서 해당 권한을 직접 허용하셔야 합니다.")
-                    .setNeutralButton("설정"
-                    ) { dialogInterface, i ->
-                        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-                        intent.data = Uri.parse("package:$packageName")
-                        startActivity(intent)
-                    }
-                    .setPositiveButton("확인"
-                    ) { dialogInterface, i -> finish() }
-                    .setCancelable(false)
-                    .create()
-                    .show()
+                Log.d("TAG", "권한부여 거절")
+                finish()
+                Toast.makeText(applicationContext, R.string.permission_denied_message, Toast.LENGTH_LONG).show()
             }
         }
     }
